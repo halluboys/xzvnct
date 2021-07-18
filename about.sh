46621e39a720a55738c88d774689654dc961208e
halluboys <87511503+halluboys@users.noreply.github.com>
2021-07-18 20:49:25 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#                 HALUBOY                       #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By HALUBOYS                             #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# HALLUBOY                                      #"
+echo -e "# ALLAH S.W.T                                   #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "#                                               #"
+echo -e "# XZVNCT.MY.ID                                  #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "                   BITCH                        #"
+echo -e "================================================="
