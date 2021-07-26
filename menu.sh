8c465c6899000e1acf7b8e183bf1273788aa7bd4
halluboys <87511503+halluboys@users.noreply.github.com>
2021-07-26 13:51:18 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -47,7 +47,7 @@ echo -e   "  \e[1;32m-----------------------------------------------------------
 echo -e   "   x)   Exit" | lolcat
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   ""
-read -p "     Select From Options [1-8 or x] :  " menu | lolcat
+read -p "     Select From Options [1-8 or x] :  " menu
 echo -e   ""
 case $menu in
 1)
