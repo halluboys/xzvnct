fd482500b68505564c51a53ab2a67ccca5ceef83
halluboys <87511503+halluboys@users.noreply.github.com>
2021-07-26 12:21:38 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -12,7 +12,7 @@ IPVPS=$(curl -s ipinfo.io/ip )
 	echo -e "   \e[032;1mCity:\e[0m $CITY"
 	echo -e "   \e[033;1mIPVPS:\e[0m $IPVPS"
 echo -e  ""
-echo -e  "   -------------------------MENU OPTIONS------------------------"
+echo -e  "   -------------------------MENU OPTIONS------------------------" | lolcat
 echo -e   "   1\e[1;33m)\e[m SSH & OpenVPN Menu"
 echo -e   "   2\e[1;33m)\e[m Panel Wireguard "
 echo -e   "   3\e[1;33m)\e[m Panel L2TP & PPTP Account"
@@ -21,9 +21,9 @@ echo -e   "   5\e[1;33m)\e[m Panel SSR & SS Account"
 echo -e   "   6\e[1;33m)\e[m Panel V2Ray"
 echo -e   "   7\e[1;33m)\e[m Panel VLess"
 echo -e   "   8\e[1;33m)\e[m Panel TRojan"
-echo -e   "  \e[1;32m------------------------------------------------------------\e[m"
-echo -e   "                             SYSTEM MENU\e[m"
-echo -e   "  \e[1;32m------------------------------------------------------------\e[m"
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
+echo -e   "                             SYSTEM MENU\e[m" | lolcat
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "   9\e[1;33m)\e[m   Add Subdomain Host For VPS"
 echo -e   "   10\e[1;33m)\e[m  Renew Certificate V2RAY"
 echo -e   "   11\e[1;33m)\e[m  Change Port All Account"
@@ -43,11 +43,11 @@ echo -e   "   24\e[1;33m)\e[m  Cloudflare Add-Ons"
 echo -e   "   25\e[1;33m)\e[m  Pointing BUG"
 echo -e   "   26\e[1;33m)\e[m  Clear log"
 echo -e   "   27\e[1;33m)\e[m  Auto Reboot"
-echo -e   "  \e[1;32m------------------------------------------------------------\e[m"
-echo -e   "   x)   Exit"
-echo -e   "  \e[1;32m------------------------------------------------------------\e[m"
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
+echo -e   "   x)   Exit" | lolcat
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   ""
-read -p "     Select From Options [1-8 or x] :  " menu
+read -p "     Select From Options [1-8 or x] :  " menu 
 echo -e   ""
 case $menu in
 1)
