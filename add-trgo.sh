c91df6a86af1521925e3f8084a5d954509576a07
halluboys <87511503+halluboys@users.noreply.github.com>
2021-07-31 17:18:16 +0700

    Update add-trgo.sh

--- add-trgo.sh
+++ add-trgo.sh
@@ -2,16 +2,6 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl http://lnd.red-flat.my.id:81/shahwjusisjsnajakajannaiajanajam | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 uuid=$(cat /proc/sys/kernel/random/uuid)
 source /var/lib/premium-script/ipvps.conf
