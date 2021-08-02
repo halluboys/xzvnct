37db6d4ef7c615922b7b09a5bb13815c295149d7
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-02 18:43:43 +0700

    Update add-trgo.sh

--- add-trgo.sh
+++ add-trgo.sh
@@ -2,6 +2,8 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
 clear
 uuid=$(cat /proc/sys/kernel/random/uuid)
 source /var/lib/premium-script/ipvps.conf
@@ -26,7 +28,7 @@ sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go
-trojangolink="trojan-go://${uuid}@${domain}:${trojango}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#${user}"
+trojangolink="trojan-go://${user}@${domain}:${trojango}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#${user}"
 clear
 echo -e "---------------TROJAN-GO ACCOUNT-------------"
 echo -e "Remarks        = ${user}"
