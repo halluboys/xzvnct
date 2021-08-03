a7c7431a83d96ec030f114017a41797833a31866
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-03 18:41:27 +0700

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -18,8 +18,8 @@ touch /etc/trojan/akun.conf
 # install v2ray
 wget https://raw.githubusercontent.com/halluboys/xzvnct/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
-bash -c "$(wget -O- https://raw.githubusercontent.com/McDull-GitHub/trojan-go-quickstart/main/quickstart.sh)"
-mkdir /root/.acme.sh
+bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
+mkdir /root/.acme.sh 
 curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
 chmod +x /root/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
