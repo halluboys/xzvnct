251b696f34d0316afc5ec7311071cf76ec18598b
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-18 20:04:45 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ wget -O vleess "https://raw.githubusercontent.com/halluboys/xzvnct/main/update/v
 wget -O wgr "https://raw.githubusercontent.com/halluboys/xzvnct/main/update/wgr.sh"
 wget -O menu "https://raw.githubusercontent.com/halluboys/xzvnct/main/update/menu.sh"
 wget -O ssh-vpn "https://raw.githubusercontent.com/halluboys/xzvnct/main/ssh-vpn.sh"
-
+wget -O ins-trojango "https://raw.githubusercontent.com/halluboys/xzvnct/main/ins-trojango.sh"
 chmod +x v2raay
 chmod +x bbr
 chmod +x trojaan
@@ -37,4 +37,5 @@ chmod +x wgr
 chmod +x ssssr
 chmod +x ssh
 chmod +x l2tp
+chmod +x ins-trojango
 echo "done"
