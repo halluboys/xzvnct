2b9a9eba0b26fd27dccecaa93bfa723c2062b30f
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-18 19:02:05 +0700

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,22 +2,20 @@
 
 clear
 echo -e "=================================================" | lolcat
-echo -e "#                 HALUBOY                       #" | lolcat
+echo -e "#                   HALUBOY                     #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
 echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
-echo -e "# For VPS with KVM and VMWare virtualization    #" | lolcat
-echo -e "# Build Up By HALUBOYS                          #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "# xxxxxxxxx                                     #" | lolcat
+echo -e "# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
 echo -e "#                                               #" | lolcat
 echo -e "#                                               #" | lolcat
 echo -e "#                                               #" | lolcat
+echo -e "#                    XZVNCT                     #" | lolcat
 echo -e "#                                               #" | lolcat
 echo -e "#                                               #" | lolcat
-echo -e "# XZVNCT                                        #" | lolcat
 echo -e "#                                               #" | lolcat
 echo -e "#-----------------------------------------------#" | lolcat
-echo -e "                   BITCH                        #" | lolcat
+echo -e "                     BITCH                      #" | lolcat
 echo -e "=================================================" | lolcat
