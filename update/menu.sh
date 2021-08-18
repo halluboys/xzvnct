0224153558b5d0de7b286e81ac28453bd343f3df
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-18 19:12:54 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -13,42 +13,42 @@ IPVPS=$(curl -s ipinfo.io/ip )
 	echo -e "   \e[033;1mIPVPS:\e[0m $IPVPS" | lolcat
 echo -e  ""
 echo -e  "   -------------------------MENU OPTIONS------------------------" | lolcat
-echo -e   "   1\e[1;33m)\e[m SSH & OpenVPN Menu"
-echo -e   "   2\e[1;33m)\e[m Panel Wireguard "
-echo -e   "   3\e[1;33m)\e[m Panel L2TP & PPTP Account"
-echo -e   "   4\e[1;33m)\e[m Panel SSTP  Account"
-echo -e   "   5\e[1;33m)\e[m Panel SSR & SS Account"
-echo -e   "   6\e[1;33m)\e[m Panel V2Ray"
-echo -e   "   7\e[1;33m)\e[m Panel VLess"
-echo -e   "   8\e[1;33m)\e[m Panel TRojan"
+echo -e   "   1\e[1;33m)\e[m SSH & OpenVPN Menu" | lolcat
+echo -e   "   2\e[1;33m)\e[m Panel Wireguard " | lolcat
+echo -e   "   3\e[1;33m)\e[m Panel L2TP & PPTP Account" | lolcat
+echo -e   "   4\e[1;33m)\e[m Panel SSTP  Account" | lolcat
+echo -e   "   5\e[1;33m)\e[m Panel SSR & SS Account" | lolcat
+echo -e   "   6\e[1;33m)\e[m Panel V2Ray" | lolcat
+echo -e   "   7\e[1;33m)\e[m Panel VLess" | lolcat
+echo -e   "   8\e[1;33m)\e[m Panel TRojan" | lolcat
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "                             SYSTEM MENU\e[m" | lolcat 
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
-echo -e   "   9\e[1;33m)\e[m   Add Subdomain Host For VPS"
-echo -e   "   10\e[1;33m)\e[m  Renew Certificate V2RAY"
-echo -e   "   11\e[1;33m)\e[m  Change Port All Account"
-echo -e   "   12\e[1;33m)\e[m  Autobackup Data VPS"
-echo -e   "   13\e[1;33m)\e[m  Backup Data VPS"
-echo -e   "   14\e[1;33m)\e[m  Restore Data VPS"
-echo -e   "   15\e[1;33m)\e[m  Webmin Menu"
-echo -e   "   16\e[1;33m)\e[m  Limit Bandwith Speed Server"
-echo -e   "   17\e[1;33m)\e[m  Check Usage of VPS Ram" 
-echo -e   "   18\e[1;33m)\e[m  Reboot VPS"
-echo -e   "   19\e[1;33m)\e[m  Speedtest VPS"
-echo -e   "   20\e[1;33m)\e[m  Information Display System" 
-echo -e   "   21\e[1;33m)\e[m  Info Script Auto Install"
-echo -e   "   22\e[1;33m)\e[m  Install BBR"
-echo -e   "   23\e[1;33m)\e[m  Add ID Cloudflare"
-echo -e   "   24\e[1;33m)\e[m  Cloudflare Add-Ons"
-echo -e   "   25\e[1;33m)\e[m  Pointing BUG"
-echo -e   "   26\e[1;33m)\e[m  Clear log"
-echo -e   "   27\e[1;33m)\e[m  Auto Reboot"
-echo -e   "   28\e[1;33m)\e[m  Update Lates Script"
+echo -e   "   9\e[1;33m)\e[m   Add Subdomain Host For VPS" | lolcat
+echo -e   "   10\e[1;33m)\e[m  Renew Certificate V2RAY" | lolcat
+echo -e   "   11\e[1;33m)\e[m  Change Port All Account" | lolcat
+echo -e   "   12\e[1;33m)\e[m  Autobackup Data VPS" | lolcat
+echo -e   "   13\e[1;33m)\e[m  Backup Data VPS" | lolcat
+echo -e   "   14\e[1;33m)\e[m  Restore Data VPS" | lolcat
+echo -e   "   15\e[1;33m)\e[m  Webmin Menu" | lolcat
+echo -e   "   16\e[1;33m)\e[m  Limit Bandwith Speed Server" | lolcat
+echo -e   "   17\e[1;33m)\e[m  Check Usage of VPS Ram"  | lolcat
+echo -e   "   18\e[1;33m)\e[m  Reboot VPS" | lolcat
+echo -e   "   19\e[1;33m)\e[m  Speedtest VPS" | lolcat
+echo -e   "   20\e[1;33m)\e[m  Information Display System" | lolcat
+echo -e   "   21\e[1;33m)\e[m  Info Script Auto Install" | lolcat
+echo -e   "   22\e[1;33m)\e[m  Install BBR" | lolcat
+echo -e   "   23\e[1;33m)\e[m  Add ID Cloudflare" | lolcat
+echo -e   "   24\e[1;33m)\e[m  Cloudflare Add-Ons" | lolcat
+echo -e   "   25\e[1;33m)\e[m  Pointing BUG" | lolcat
+echo -e   "   26\e[1;33m)\e[m  Clear log" | lolcat
+echo -e   "   27\e[1;33m)\e[m  Auto Reboot" | lolcat
+echo -e   "   28\e[1;33m)\e[m  Update Lates Script" | lolcat
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "   x)   Exit" | lolcat
 echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   ""
-read -p "     Select From Options [1-8 or x] :  " menu
+read -p "     Select From Options [1-8 or x] :  " menu | lolcat
 echo -e   ""
 case $menu in
 1)
