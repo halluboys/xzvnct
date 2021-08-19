4979b9bc78a6741a100207d4d9fe70a96d7c4e16
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-19 12:18:21 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -28,6 +28,7 @@ wget -O wgr "https://raw.githubusercontent.com/halluboys/xzvnct/main/update/wgr.
 wget -O menu "https://raw.githubusercontent.com/halluboys/xzvnct/main/update/menu.sh"
 wget -O ssh-vpn "https://raw.githubusercontent.com/halluboys/xzvnct/main/ssh-vpn.sh"
 wget -O ins-trojango "https://raw.githubusercontent.com/halluboys/xzvnct/main/ins-trojango.sh"
+wget -O add-trgo "https://raw.githubusercontent.com/halluboys/xzvnct/main/update/add-trgo.sh"
 chmod +x v2raay
 chmod +x bbr
 chmod +x trojaan
@@ -38,4 +39,5 @@ chmod +x ssssr
 chmod +x ssh
 chmod +x l2tp
 chmod +x ins-trojango
+chmod +x add-trgo
 echo "done"
