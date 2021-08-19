5e471b705d34fca67c9cde900dab41f950478cf7
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-19 15:47:49 +0700

    Update trojaan.sh

--- update/trojaan.sh
+++ update/trojaan.sh
@@ -12,13 +12,12 @@ echo -e " 1)  Create Trojan Account"
 echo -e " 2)  Deleting Trojan Account"
 echo -e " 3)  Renew Trojan Account"
 echo -e " 4)  Check User Login Trojan"
-echo -e " 5)  Create TrojanGo Account"
 echo -e ""
 echo -e "\e[1;32m══════════════════════════════════════════\e[m" | lolcat
 echo -e " x)   MENU"
 echo -e "\e[1;32m══════════════════════════════════════════\e[m" | lolcat
 echo -e ""
-read -p "     Please Input Number  [1-5 or x] :  "  trojan
+read -p "     Please Input Number  [1-4 or x] :  "  trojan
 echo -e ""
 case $trojan in
 1)
@@ -32,9 +31,6 @@ renew-tr
 ;;
 4)
 cek-tr
-;;
-5)
-add-trgo
 ;;
 x)
 menu
