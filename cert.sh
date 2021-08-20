296172fded890eeae09e44325d05ced34f9cbe23
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-20 10:28:06 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
-clear
 echo start
 sleep 0.5
 source /var/lib/premium-script/ipvps.conf
