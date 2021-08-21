f160fe4df530cdf028cf9c5d6b71716009bddb92
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-21 16:55:30 +0700

    Update add-trgo.sh

--- update/add-trgo.sh
+++ update/add-trgo.sh
@@ -5,14 +5,14 @@ NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 clear
-uuid=$(cat /proc/sys/kernel/random/uuid)
+uuid=$(cat /etc/trojan-go/uuid.txt)
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
 fi
-trojango="$(cat ~/log-install.txt | grep -i TrojanGO | cut -d: -f2|sed 's/ //g')"
+trojango="$(cat ~/log-install.txt | grep -i TrojanGo | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 		read -rp "Password : " -e user
 		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
@@ -28,7 +28,7 @@ sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go
-trojangolink="trojan-go://${user}@${domain}:${trojango}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#${user}"
+trojangolink="trojan-go://${user}@${domain}:${trojango}/?sni=${domain}&type=ws&host=${domain}&path=/xzvnct&encryption=none#${user}"
 clear
 echo -e "---------------TROJAN-GO ACCOUNT-------------"
 echo -e "Remarks        = ${user}"
@@ -36,6 +36,6 @@ echo -e "IP / Host      = ${domain}"
 echo -e "Port           = ${trojango}"
 echo -e "Key            = ${user}"
 echo -e "Expired        = $exp"
-echo -e "-----------------------------------------------"
+echo -e "-------------------------------------------------------------------"
 echo -e "Link TROJAN-GO : ${trojangolink}"
-echo -e "-----------------------------------------------"
+echo -e "-------------------------------------------------------------------"
