bfc1e3412884de9867d703ebee6e6eb3badb936e
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-23 18:26:30 +0700

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -1,10 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
+echo "Checking VPS"
+clear
+cat /usr/bin/bannerku | lolcat
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
 fi
+tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "User: " -e user
 		CLIENT_EXISTS=$(grep -w $user /etc/v2ray/config.json | wc -l)
@@ -27,11 +36,11 @@ cat>/etc/v2ray/$user-tls.json<<EOF
       "v": "2",
       "ps": "${user}",
       "add": "${domain}",
-      "port": "8443",
+      "port": "${tls}",
       "id": "${uuid}",
       "aid": "2",
       "net": "ws",
-      "path": "/xzvnct",
+      "path": "/v2ray",
       "type": "none",
       "host": "",
       "tls": "tls"
@@ -42,32 +51,35 @@ cat>/etc/v2ray/$user-none.json<<EOF
       "v": "2",
       "ps": "${user}",
       "add": "${domain}",
-      "port": "80",
+      "port": "${none}",
       "id": "${uuid}",
       "aid": "2",
       "net": "ws",
-      "path": "/xzvnct",
+      "path": "/v2ray",
       "type": "none",
       "host": "",
       "tls": "none"
 }
 EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
 vmesslink1="vmess://$(base64 -w 0 /etc/v2ray/$user-tls.json)"
 vmesslink2="vmess://$(base64 -w 0 /etc/v2ray/$user-none.json)"
 systemctl restart v2ray
 systemctl restart v2ray@none
+service cron restart
 clear
 echo -e ""
 echo -e "==========-V2RAY/VMESS-=========="
 echo -e "Remarks        : ${user}"
 echo -e "Domain         : ${domain}"
-echo -e "port TLS       : 8443"
-echo -e "port none TLS  : 80"
+echo -e "port TLS       : ${tls}"
+echo -e "port none TLS  : ${none}"
 echo -e "id             : ${uuid}"
 echo -e "alterId        : 2"
 echo -e "Security       : auto"
 echo -e "network        : ws"
-echo -e "path           : /xzvnct",
+echo -e "path           : /xzvnct"
 echo -e "================================="
 echo -e "link TLS       : ${vmesslink1}"
 echo -e "================================="
