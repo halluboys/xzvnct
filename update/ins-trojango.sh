98f2d59ee230e15d34e1687c1d90927b18d9dbe7
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-24 11:23:00 +0700

    Update ins-trojango.sh

--- update/ins-trojango.sh
+++ update/ins-trojango.sh
@@ -27,7 +27,7 @@ cat <<EOF > /etc/trojan-go/config.json
     "local_addr": "0.0.0.0",
     "local_port": 2096,
     "remote_addr": "127.0.0.1",
-    "remote_port": 81,
+    "remote_port": 443,
     "log_level": 1,
     "log_file": "/var/log/trojan-go.log",
     "password": [
