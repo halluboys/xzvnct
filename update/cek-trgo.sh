e160a63f264b3989eafdcae6158368e2d2f0577b
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-24 12:25:23 +0700

    Update cek-trgo.sh

--- update/cek-trgo.sh
+++ update/cek-trgo.sh
@@ -2,27 +2,41 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
+MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-data=( `cat /var/log/trojan-go.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
-echo "------------------------------------";
-echo "-----=[ TrojanGO User Login ]=-----";
-echo "------------------------------------";
+clear
+echo -n > /tmp/other.txt
+data=( `cat /etc/trojan-go/config.json | grep '^###' | cut -d ' ' -f 2`);
+echo "-------------------------------";
+echo "-----=[ TrojanGo User Login ]=-----";
+echo "-------------------------------";
 for akun in "${data[@]}"
 do
-data2=( `lsof -n | grep -i ESTABLISHED | grep trojan-go | awk '{print $9}' | cut -d':' -f2 | grep -w 2096 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
 echo -n > /tmp/iptrojan-go.txt
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep v2ray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-jum=$(cat /var/log/trojan-go.log | grep -w $akun | awk '{print $4}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+jum=$(cat /var/log/trojan-go/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+if [[ "$jum" = "$ip" ]]; then
+echo "$jum" >> /tmp/iptrojan-go.txt
+else
+echo "$ip" >> /tmp/other.txt
+fi
+jum2=$(cat /tmp/iptrojan-go.txt)
+sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
+done
+jum=$(cat /tmp/iptrojan-go.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
-else
-echo "$jum" > /tmp/iptrojan-go.txt
-fi
-done
-jum2=$(cat /tmp/iptrojan-go.txt | nl)
-echo "user : $akun";
-echo "$jum2";
 echo "-------------------------------"
+fi
+rm -rf /tmp/iptrojan-go.txt
 done
+oth=$(cat /tmp/other.txt | sort | uniq | nl)
+echo "other";
+echo "$oth";
+echo "-------------------------------"
+rm -rf /tmp/other.txt
