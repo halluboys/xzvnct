720da0a69485056bc7fa9c225d2a4bc16a959f8b
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-25 13:46:34 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -190,9 +190,9 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/halluboys/xzvnct/main/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/halluboys/px/main/nginx.conf"
 mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/halluboys/xzvnct/main/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/halluboys/px/main/vps.conf"
 /etc/init.d/nginx restart
 
 # install badvpn
