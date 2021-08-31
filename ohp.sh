244ef14128979d502a6dbbf692c5487ac9cd2267
halluboys <87511503+halluboys@users.noreply.github.com>
2021-08-31 13:18:40 +0700

    Create ohp.sh

---
+++ ohp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/halluboys/xzvnct/main/ohp-db.sh;chmod +x ohp-db.sh;./ohp-db.sh
+wget https://raw.githubusercontent.com/halluboys/xzvnct/main/ohp-ovpn.sh;chmod +x ohp-ovpn.sh;./ohp-ovpn.sh
