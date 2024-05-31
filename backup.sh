9c9008d19787043856bd759ddc4e22e076bacdd0
halluboys <87511503+halluboys@users.noreply.github.com>
2024-05-31 18:21:24 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -15,20 +15,7 @@ mkdir /root/backup
 sleep 1
 echo Start Backup
 clear
-cp /etc/passwd backup/
-cp /etc/group backup/
-cp /etc/shadow backup/
-cp /etc/gshadow backup/
-cp -r /etc/wireguard backup/wireguard
-cp /etc/ppp/chap-secrets backup/chap-secrets
-cp /etc/ipsec.d/passwd backup/passwd1
-cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
-cp -r /var/lib/premium-script/ backup/premium-script
-cp -r /home/sstp backup/sstp
-cp -r /etc/v2ray backup/v2ray
-cp -r /etc/trojan backup/trojan
-cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
-cp -r /home/vps/public_html backup/public_html
+cp -r /etc/xray/config.json backup/config.json
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
